353db5dd5e2fac21675bf466d7ce876636c27330
tobarrr <vicente.tobar.r@ug.uchile.cl>
2025-09-28 21:30:55 -0300

    Update test.ino

--- motor/test.ino
+++ motor/test.ino
@@ -1,2 +1,42 @@
-//hola
-int s = 4;
+#define PINBUZZER 10
+#define PINBOTON 2
+#define PINLED 13
+#define PIN_Sensor_ON 11
+
+void setup() {
+  Serial.begin(115200);
+  Peripherals_init();
+  TB6612FNG_init();
+  Sensors_init();
+
+  digitalWrite(PINLED, LOW);
+  delay(500);
+
+  Motores(0, 0);
+  Serial.println("Ahora, nos aseguraremos que los motores funcionan de manera correcta :3");
+  Serial.println("Instrucciones:   ");
+  Serial.println("");
+  delay(100);
+}
+
+
+void loop() {
+  Serial.println("Presiona el botón, ambos motores deberían moverse hacia adelante. Si uno o ambos van para atrás, cambia la polarización de los cables ;P.");
+  Serial.println("");
+  
+  delay(500);
+  WaitBoton();
+
+  Motores(50, 50);
+
+  delay(1000);
+
+  Serial.println("---o---");
+
+  Serial.println("Presiona otra vez el botón para detener los motores");
+  Serial.println("");
+  WaitBoton();
+  Motores(0, 0);
+  delay(1000);
+  
+}
